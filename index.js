b1ae8e06812e8b148e7687f05bc3438822e1fa7c
Alifend <andres.celis@correounivalle.edu.co>
2021-08-21 19:06:08 -0500

    .

--- index.js
+++ index.js
@@ -10,11 +10,17 @@ const registerRouter = require("./routes/registerRouter");
 const loginRouter = require("./routes/loginRouter");
 // Middlewares
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://www.localhost:3000",
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 //app.use("/tasks", taskRouter);
 app.use("/login", loginRouter);
 app.use("/register", registerRouter);
